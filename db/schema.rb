68db31ba9fd3562ae39fc4b70114cd86fbc8969a
sadattachi <nazarii200322@gmail.com>
2022-07-15 19:44:19 +0300

    Add doctor_speciality table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_163555) do
+ActiveRecord::Schema.define(version: 2022_07_15_163902) do
+
+  create_table "doctor_specialities", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
