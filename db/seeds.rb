ca3ef4ee4d0d53b39a0530636aadee329761a1a8
sadattachi <nazarii200322@gmail.com>
2022-07-15 19:37:58 +0300

    Add roles table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.create([{ name: 'Patient' }, { name: 'Doctor' }, { name: 'Admin' }])
