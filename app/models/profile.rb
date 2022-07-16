7faab2ce3066d351147e783580485a892de72141
sadattachi <nazarii200322@gmail.com>
2022-07-16 13:41:27 +0300

    Fix associations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
   belongs_to :doctor_speciality
+  has_one_attached :avatar
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,3 @@
 class Role < ApplicationRecord
-  has_one_attached :avatar
+  has_many :users
 end
