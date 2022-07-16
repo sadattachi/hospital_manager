706685637294141a2e180965c72dd7fe67f38673
sadattachi <nazarii200322@gmail.com>
2022-07-16 18:14:57 +0300

    Add doctor_specialities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  resources :doctor_specialities, only: %i[new create]
+  # get '/speciality/new', to: 'speciality#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
