209417bf5df29a931e314e569c71b49a9633eae0
sadattachi <nazarii200322@gmail.com>
2022-07-18 17:24:03 +0300

    Add create action for appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,21 @@ class AppointmentsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
   def index
-    @appointments = current_user.doctor_appointments
+    @appointments = if current_user.patient?
+                      current_user.doctor_appointments
+                    else
+                      current_user.patient_appointments
+                    end
+  end
+
+  def create
+    @appointment = Appointment.new(patient_id: current_user.id,
+                                   doctor_id: params[:id].to_i,
+                                   status: 'Active')
+    if @appointment.save && User.find_by(id: params[:id].to_i).patient_appointments.count <= 10
+      redirect_to appointments_path, notice: 'Appointment was added'
+    else
+      redirect_to root, notice: 'Cannot add appointment'
+    end
   end
 end
