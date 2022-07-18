29634e679898070bc2b94279bd631d44fb4f9be9
sadattachi <nazarii200322@gmail.com>
2022-07-18 17:26:45 +0300

    Add route to create action for appointments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :doctor_specialities, only: %i[new create]
   resources :profile, only: %i[show edit update]
   resources :appointments, only: %i[index]
+  post 'appointments/:id', to: 'appointments#create'
   # resources :user, only: %i[new create]
   get '/user/new', to: 'user#new'
   post 'user', to: 'user#create'
