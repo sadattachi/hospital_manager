758fd78817c78385761d973a7c4020c9858c0c7c
sadattachi <nazarii200322@gmail.com>
2022-07-19 19:26:09 +0300

    Assign route to custom registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'home#index'
   get '/filter', to: 'home#filter'
   resources :doctor_specialities, only: %i[new create]
