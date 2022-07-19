225e1e08914ca05132963e5e3e24afd5be2c8e86
sadattachi <nazarii200322@gmail.com>
2022-07-19 13:25:31 +0300

    Add filter action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
     @doctors = User.where('role_id = :doctor', doctor: 2)
+    @current_select = nil
+  end
+
+  def filter
+    @current_select = params[:profile][:doctor_speciality_id].to_i
+    @doctors = User.where('role_id = :doctor', doctor: 2).select do |u|
+      u.profile.doctor_speciality.id == @current_select
+    end
+
+    render :index
   end
 end
