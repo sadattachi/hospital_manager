f1b59c41fd5220bba9a4e23f10d81fca69d84781
sadattachi <nazarii200322@gmail.com>
2022-07-20 20:38:25 +0300

    Delete unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,7 @@ Rails.application.routes.draw do
   }
   root 'home#index'
   get '/filter', to: 'home#filter'
-  resources :doctor_specialities, only: %i[new create]
-  resources :profile, only: %i[show edit update]
   resources :appointments, only: %i[index update]
   post 'appointments/:id', to: 'appointments#create'
-  # resources :user, only: %i[new create]
-  get '/user/new', to: 'user#new'
-  post 'user', to: 'user#create'
-  # get '/doctor/edit', to: 'doctor#edit'
-  # get '/speciality/new', to: 'speciality#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
