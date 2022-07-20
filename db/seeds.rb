addf8d2fb0b351e9cdf5adb6c3515b5dee2e1d39
sadattachi <nazarii200322@gmail.com>
2022-07-20 13:28:47 +0300

    Setup ActiveAdmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Role.create([{ name: 'Patient' }, { name: 'Doctor' }, { name: 'Admin' }])
+if Rails.env.development?
+  AdminUser.create!(phone: '0000000000', password: 'password',
+                    password_confirmation: 'password')
+end
