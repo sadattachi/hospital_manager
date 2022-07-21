4bbbbae4be2bb546127bf2a328f28680f2a225b0
sadattachi <nazarii200322@gmail.com>
2022-07-21 16:46:02 +0300

    Add validations to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
+  validates :status, inclusion: { in: %w[Closed Active] }
+
   belongs_to :patient, class_name: 'User'
   belongs_to :doctor, class_name: 'User'
 end
