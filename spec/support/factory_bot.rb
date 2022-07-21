21661fc3b819abf94fdcb214344fcf7b7b25588f
sadattachi <nazarii200322@gmail.com>
2022-07-21 14:42:04 +0300

    Add faker for user factory

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     profile_id { 1 }
     role_id { 1 }
-    phone { '0123456789' }
-    password { 'valid_password' }
+    phone { Faker::Number.numerify('#' * 10) }
+    password { Faker::String.random(length: 7..12) }
   end
 end
